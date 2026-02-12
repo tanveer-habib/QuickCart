62b6d6f1fcc10e61a2bd394beab232773e067695
tanveer-habib <92tanveer94478@gmail.com>
2026-02-12 12:06:09 +0500

    Make a new inngest app

--- config/inngest.js
+++ config/inngest.js
@@ -3,7 +3,7 @@ import connectDB from "./db";
 import User from "@/models/User";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "quickcart-next" });
+export const inngest = new Inngest({ id: "quickcart-next-v2" });
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
